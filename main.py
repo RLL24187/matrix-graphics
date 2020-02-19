ce6291dd75239debee4ebf79784ef09eae96b3e3
rleongPd5 <rleong00@stuy.edu>
2020-02-18 23:38:52 -0500

    added test code, so ident works now

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
 
-print_matrix(matrix)
-
-m0 = new_matrix(0, 0)
 add_point(matrix, 1, 2)
-
 print_matrix(matrix)
+
+m0 = new_matrix(2, 2)
+ident(m0)
+print_matrix(m0)
+
+m1 = new_matrix(4, 3)
+add_point(m1, 3, 3)
+print_matrix(m1)
+ident(m1)
+print_matrix(m1)
 
 
 draw_lines( matrix, screen, color )
